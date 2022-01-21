6515e286e02a328075733090f14bac7ab3729f1a
Vitor M. Almeida <83554161+vitoalmeida@users.noreply.github.com>
2022-01-20 23:14:17 -0300

    organized folders

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RootStack from './src/navigation/RootStack.js'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <StatusBar style="auto" />
+  return <RootStack/>;
 }
 
 const styles = StyleSheet.create({
